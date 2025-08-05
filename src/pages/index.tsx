8475871938b5675e0ace1ada934de57c1bf971e4
ikethehippie <89872786+ikethehippie@users.noreply.github.com>
2025-08-05 11:34:44 -0500

    Delete src/pages directory

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Auction Analyzer</h1>
-      <p>Paste an auction URL here and hit "Analyze" (UI coming soon)</p>
-    </main>
-  );
-}
-
