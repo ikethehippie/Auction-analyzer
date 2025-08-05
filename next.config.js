6b339b7ab0d2eb07526a6a52bf5d2be6fa161048
ikethehippie <89872786+ikethehippie@users.noreply.github.com>
2025-08-05 13:33:25 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone'
+};
+
+module.exports = nextConfig;
